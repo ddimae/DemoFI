45f9f59b558d688bc4091fc812eab1c0ac607a0b
ddimae72 <ddimae72@gmail.com>
2023-03-25 13:38:22 +0200

    demo work UnaryOperator

--- src/demofinctinterfaces/Main.java
+++ src/demofinctinterfaces/Main.java
@@ -1,7 +1,46 @@
 package demofinctinterfaces;
 
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 5;
+        int b = 10;
+        System.out.println(sum(a, b));
+        System.out.println(sum(7, -3));
+        System.out.println(sum(3.2, 4.5));
+
+        double[] arr = new double[]{-1.5, 2., -3.9};
+
+        //#1
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Math.sqrt(arr[i]));
+        }
+        //#2
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Math.pow(arr[i], 2));
+        }
+        //#3
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Math.ceil(arr[i]));
+        }
+
+        obrabotkaArray(arr,x->Math.sqrt(x));
+        obrabotkaArray(arr,x->Math.abs(x));
+
+
     }
+
+    static double sum(double a, double b) {
+        return a + b;
+    }
+
+    static void obrabotkaArray(double[] arr, UnaryOperator<Double> myOper) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(myOper.apply(arr[i]));
+        }
+    }
+
+
 }
